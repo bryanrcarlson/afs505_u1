86d126ca4f6ccd89bbd9f89b78ae9b1027ff8197
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-01-23 11:47:33 -0800

    lect activities

---
+++ lectures/2020-01-23/1.py
@@ -0,0 +1,16 @@
+#print("How old are you? ", end='')
+#age = input()
+#
+#print("How tall are you? ", end='')
+#height = input()
+#
+#print("How much do you weight? ", end='')
+#weight = input()
+#
+#print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
+
+from sys import argv
+
+script, age, height, weight = argv
+
+print(f"So, you're {age} old, {height} tall, and {weight} heavy.")
