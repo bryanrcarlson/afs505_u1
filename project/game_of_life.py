d6822a02cb298eaed57a3714cb5e5f672ad032f5
Bryan Carlson <bryan.r.carlson@gmail.com>
2020-02-12 15:44:35 -0800

    init stab at rst docstrings

--- project/game_of_life.py
+++ project/game_of_life.py
@@ -1,115 +1,169 @@
+"""Script to run Conway's Game of Life
+
+.. module:: game_of_life
+    :synopsis: project assignment for WSU AFS505 unit 1
+
+.. moduleauthor:: Bryan Carlson
+"""
+
 from sys import argv
 import copy
 
-class Board(object):
-    def __init__(self, numberRows, numberColumns):
-        self.nrow = numberRows
-        self.ncol = numberColumns
+class GameOfLife(object):
+    """Class to represent Conway's Game of Life
+    
+    :param nrow: The number of rows in the grid
+    :type nrow: int
+    :param ncol: The number of columns in the grid
+    :type ncol: int
+    :param grid: A 2-d grid of cells that are either active (true) or inactive (false)
+    :type grid: 2-d list of booleans        
+    """
+
+    def __init__(self, number_rows, number_columns):
+        """Constructor method
+        """
+        self.nrow = number_rows
+        self.ncol = number_columns
         self.grid = None
 
-    def BuildGrid(self):
+    def build_grid(self):
+        """initializes the grid using nrow and ncol attributes
+        """
+
         newGrid = []
         for row in range(self.nrow):
             currRow = []
             for col in range(self.ncol):
-                currRow.append(0)
+                currRow.append(False)
             newGrid.append(currRow)
         
         self.grid = newGrid
 
-    def ActivateCell(self, row, col):
+    def activate_cell(self, row, col):
+        """sets value in grid[row-1][int-1] to True
+
+        :param row: row number of cell to activate, 1-nrow
+        :type row: int
+        :param col: col number of cell to activate, 1-ncol
+        :type col: int
+        """
+
         if self.grid == None:
             raise Exception("Grid not initialized")
         if((row-1 < 0 | row-1 > len(self.nrow)) | (col-1 < 0 | col-1 > len(self.nrow))):
             raise Exception("Cell is outside of bounds")
-        self.grid[row-1][col-1] = 1
+        self.grid[row-1][col-1] = True
 
-    def CountNeighbors(self, row, col):
+    def count_neighbors(self, row, col):
+        """returns integer value (0-8) for number of active cells adjacent to cell specified in arguments row,col
+
+        :param row: row number of cell for which neighbors are counted, 1-nrow
+        :type row: int
+        :param col: col number of cell for which neighbors are counted, 1-ncol
+        :type col: int
+        :return: An int count of the number of active neighbors to given cell (0-8)
+        """
+
         neighbors = 0
         #NW
         if((row > 0) & (col > 0)):
-            if(self.grid[row-1][col-1] == 1):
+            if(self.grid[row-1][col-1] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found NW neighbor")
         
         #N
         if(row > 0):
-            if(self.grid[row-1][col] == 1):
+            if(self.grid[row-1][col] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found N neighbor")
 
         #NE
         if((row > 0) & (col < (self.ncol-1))):
-            if(self.grid[row-1][col+1] == 1):
+            if(self.grid[row-1][col+1] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found NE neighbor")
         
         #E
         if(col < (self.ncol-1)):
-            if(self.grid[row][col+1] == 1):
+            if(self.grid[row][col+1] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found E neighbor")
 
         #SE
         if((row < (self.nrow-1)) & (col < (self.ncol-1))):
-            if(self.grid[row+1][col+1] == 1):
+            if(self.grid[row+1][col+1] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found SE neighbor")
         
         #S
         if(row < (self.nrow-1)):
-            if(self.grid[row+1][col] == 1):
+            if(self.grid[row+1][col] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found S neighbor")
 
         #SW
         if((row < (self.nrow-1)) & (col > 0)):
-            if(self.grid[row+1][col-1] == 1):
+            if(self.grid[row+1][col-1] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found SW neighbor")
 
         #W
         if(col > 0):
-            if(self.grid[row][col-1] == 1):
+            if(self.grid[row][col-1] == True):
                 neighbors += 1
-                #print(f"{row+1}:{col+1} found W neighbor")
 
         return neighbors
 
-    def SwitchCell(self, row, col, neighbors, nextGrid):
-        if(self.grid[row][col] == 0):
+    def switch_cell(self, row, col, neighbors, nextGrid):
+        """follows conways game of life logic to activate/deactivate a cell in nextGrid based on number of neighbors
+
+        :param row: row number of cell to switch, 1-nrow
+        :type row: int
+        :param col: col number of cell to switch, 1-ncol
+        :type col: int
+        :param neighbors: number of active neighbors that cell has
+        :type neighbors: int
+        :param nextGrid: represents the grid of the future timestep, changes to cell activation are made to this
+        :type nextGrid: 2-d list of booleans
+        """
+
+        if(self.grid[row][col] == False):
             if(neighbors == 3):
-                nextGrid[row][col] = 1
+                nextGrid[row][col] = True
         else:
             if(neighbors < 2):
-                nextGrid[row][col] = 0
+                nextGrid[row][col] = False
             elif(neighbors > 3):
-                nextGrid[row][col] = 0
+                nextGrid[row][col] = False
 
-    def Step(self):
-        #nextGrid = self.grid[:]
-        #nextGrid = self.grid.copy()
+    def step(self):
+        """moves the simulation forward a single tick by updating all cells in the grid and prints the updated grid
+        """
+
         nextGrid = copy.deepcopy(self.grid)
 
         for row in range(self.nrow):
             for col in range(self.ncol):
-                neighbors = self.CountNeighbors(row, col)
-                self.SwitchCell(row, col, neighbors, nextGrid)
+                neighbors = self.count_neighbors(row, col)
+                self.switch_cell(row, col, neighbors, nextGrid)
         self.grid = copy.deepcopy(nextGrid)
-        self.PrintGrid()
+        self.print_grid()
 
-    def PrintGrid(self):
+    def print_grid(self):
+        """prints the grid to stdout representing active cells as "*" and inactive as "-"
+        """
+
         if self.grid == None:
             raise Exception("Grid not initialized")
         for row in self.grid:
             for cell in row:
-                if cell == 0:
-                    print("-", end="")
-                else:
+                if cell:
                     print("*", end = "")
+                else:
+                    print("-", end="")
+                    
             print("\n", end = "")
 
-def ParseCellArg(cellStr):
+def parse_cell_arg(cellStr):
+    """Takes a string in "#:#" format and converts it to a dictionary
+
+    :return: A dictionary of {"row":int, "col":int} where row is number to left of ":" and col is number to right of ":" in cellStr
+    """
+
     dims = cellStr.split(":")
     if(len(dims) != 2):
         raise Exception("Error parsing cell")
@@ -122,22 +176,31 @@ def ParseCellArg(cellStr):
     return cell
             
 def main(argv):
+    """Composition root to handle command line arguments and initialize classes
+
+    :param argv: list of strings following call to script. argv[1] is assumed to be number of steps/ticks taken followed by 0-many strings to specify active cells in format #:# (e.g. 1:1 will activate the top-left cell of the grid)
+    :type argv: list of strings
+    """
+
+    GRID_HEIGHT = 30
+    GRID_WIDTH = 80
+
     # Build the board and grid
     ticks = int(argv[1])
-    board = Board(30, 80)
-    board.BuildGrid()
+    board = GameOfLife(GRID_HEIGHT, GRID_WIDTH)
+    board.build_grid()
 
     # Set initial conditions
     i = 2
     while i < len(argv):
-        cell = ParseCellArg(argv[i])
-        board.ActivateCell(cell["row"], cell["col"])
+        cell = parse_cell_arg(argv[i])
+        board.activate_cell(cell["row"], cell["col"])
         i += 1
-    board.PrintGrid()
+    board.print_grid()
 
     stepCount = 0
     while(stepCount < ticks):
-        board.Step()
+        board.step()
         stepCount += 1
 
 if __name__ == "__main__":
